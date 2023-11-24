9e70c18cba9c8a18185703577a4b139e4cc65edb
rohankadu <guitaristrohan14@gmail.com>
2023-11-24 17:13:47 +0530

    Test0

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "s3" {
-    bucket = "tf-backend-rk-test-123"
-    profile = "default"
-    key = "state/terraform.tfstate"
-    encrypt = true
-    region = var.region  
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "tf-backend-rk-test-123"
+#     profile = "default"
+#     key = "state/terraform.tfstate"
+#     encrypt = true
+#     region = var.region  
+#   }
+# }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,15 @@
 variable "region" {
   description = "AWS Region"
   default = "us-east-1"
-  type = string
 }
 
 variable "profile" {
   description = "AWS profile to assume permissions to deploy terraform"
   default = "default"
-  type = string
 }
 
 variable "ami_id" {
   description = "AMI ID for EC2 Instance"
-  default = ""
-  type = string
 }
 
 variable "instance_type" {
@@ -24,28 +20,18 @@ variable "instance_type" {
 
 variable "subnet_id" {
   description = "Subnet id"
-  type = string
 }
 
 variable "volume_size" {
   description = "Size of the volume in gibibytes (GiB)."
-  type = number
-  default = 8
+  default = "8"
 }
 
 variable "volume_type" {
   description = "Type of volume. Valid values include standard, gp2, gp3, io1, io2, sc1, or st1. Defaults to gp2"
-  type = string
   default = "gp2"
 }
 
 variable "security_groups" {
   description = "List of security group names to associate with."
-  type = list()
-}
-
-
-variable "vpc_id" {
-  description = "AWS VPC Id"
-  type = string
 }
