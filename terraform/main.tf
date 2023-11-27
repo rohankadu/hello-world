2c114272f2bd53ffe083ff700880831e87b00c74
rohankadu <guitaristrohan14@gmail.com>
2023-11-27 11:49:55 +0530

    Fixing terraform.tfvars and main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "web" {
   ami           = var.ami_id
   instance_type = var.instance_type
   subnet_id = var.subnet_id
-  security_groups = [ var.security_groups ]
+  security_groups = var.security_groups
   root_block_device {
     volume_size = var.volume_size
     volume_type = var.volume_type
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -5,4 +5,4 @@ instance_type = "t3.micro"
 subnet_id = "subnet-08f772fcafd5c8471"
 volume_size = 8
 volume_type = "gp2"
-security_groups = [sg-0d667ac7df234a103]
+security_groups = ["sg-01da1d8712f687b2c"]
